b4aa86077b68b09ef151914ba24beccd8b04acc2
Saepul Bahri <bahreesaepul1@gmail.com>
2023-12-20 15:06:48 +0700

    made index js to handle client functionality instead of server

--- session-10/index.js
+++ session-10/index.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const studentController = require('./controllers/studentController');
+const BASE_URL = 'http://localhost:3000/mahasiswa';
 
-const app = express();
-const PORT = 3000;
-
-app.use('/', studentController);
-
-app.listen(PORT, () => {
-  console.log('🟢 Server is running on port:', PORT);
-});
+async function addStudent(payload) {
+  try {
+    const response = await fetch(BASE_URL + '/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    }).then((res) => res.json());
+    console.log(response);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
